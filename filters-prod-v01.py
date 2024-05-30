ad9b5180261bd56fcf0875ef9a1045603dac34b3
bilelsalem <45262839+bilelsalem@users.noreply.github.com>
2024-05-30 13:50:03 +0100

    Update filters-prod-v01.py

--- filters-prod-v01.py
+++ filters-prod-v01.py
@@ -16,17 +16,21 @@ current_tenant = None
 current_filter = None
 current_epg = None
 dst_to_port = None
-
-
+x=0
 for row in sheet.iter_rows(min_row=2, values_only=True):
     tenant_name, filter_name, entry_name, ethertype, protocole, dst_from_port, dst_to_port, stateful = row
    
     if current_tenant != tenant_name:
         current_tenant = tenant_name
-        data["apic"]["tenants"].append({"name": tenant_name, "filters": []}
+        x=1
+        data["apic"]["tenants"].append({"name": tenant_name, "filters": []})
 
-    data["apic"]["tenants"][-1]["filters"][-1]["entries"].append({"name": entry_name, "ethertype": ethertype, "protocol":protocole, "destination_from_port":dst_from_port, "stateful": stateful})
-
+    if current_filter != filter_name or x==1:
+        current_filter = filter_name
+        data["apic"]["tenants"][-1]["filters"].append({"name": filter_name,"entries": []})
+    
+    data["apic"]["tenants"][-1]["filters"][-1]["entries"].append({"name": entry_name, "ethertype": ethertype, "protocol":protocole, "destination_from_port":dst_from_port, "destination_to_port":dst_to_port, "stateful": stateful})
+    x=0
 # Write the data to a YAML file
 with open("filters.yaml", "w") as yaml_file:
     yaml.dump(data, yaml_file, Dumper=MyDumper, default_flow_style=False,sort_keys=False)
